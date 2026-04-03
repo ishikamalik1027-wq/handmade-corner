6503479b02ea06bb65dc1664a111ba52155764e2
Ishika Malik <ishikamalik1027@gmail.com>
2026-04-03 22:56:50 +0530

    fixed static folder / css issue

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static("public"));
 
 const sessionOptions = {
     secret: "mysupersecretcode",
@@ -112,6 +112,6 @@ app.use("/", userRouter);
 
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+    console.log("server is running");
 });
 
